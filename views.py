934bfc9eb2d59872d5419ac3211e708e70f56c7a
gw0 [http://gw.tnode.com/] <gw.2012@tnode.com>
2012-06-04 18:57:38 +0200

    Added and improved recommended coding style examples.

--- views.py
+++ views.py
@@ -2,5 +2,17 @@
 """
 Views for {{ app_name|title }} Django application.
 """
+from django.shortcuts import render_to_response, get_object_or_404, get_list_or_404
 
-# Create your views here.
+from {{ app_name }} import models
+
+
+# Replace the following example with your views.
+
+def home(req):
+    """View for `home` page."""
+    {{ app_name }} = get_object_or_404(models.{{ app_name|title }}, name='home')
+    return render_to_response('{{ app_name }}/home.html', {
+        '{{ app_name }}': {{ app_name }},
+    })
+
